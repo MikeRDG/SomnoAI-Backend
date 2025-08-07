3276f38eded5245d21546ece54fe22bf5fc21c27
Mikayel Kostanyan <kostanyanmike@gmail.com>
2025-08-07 18:01:25 +0400

    main.ts fix

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const udpPort = parseInt(process.env.UDP_PORT ?? '5000');
 
   const app = await NestFactory.create(AppModule);
-  await app.listen(httpPort, '0.0.0.0');
+  await app.listen(httpPort);
   console.log(`HTTP Server running on 0.0.0.0:${httpPort}`);
 
   const udpServer = new UdpServer(udpPort);
